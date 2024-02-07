98b3e0f76d5b15e14dc3b65b1782d5d9b3fe97a4
hrishikesh <hrishikeshajith0@gmail.com>
2024-02-08 02:46:24 +0530

    added router funtion for endpoint signup

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,12 +3,21 @@ package main
 import (
 	"log"
 	"server/db"
+	"server/internal/user"
+	"server/router"
 )
 
 func main() {
-	_, err := db.NewDatabase()
+	dbConn, err := db.NewDatabase()
 
 	if err != nil {
 		log.Fatalf("could not initialize database connection: %s", err)
 	}
+
+	userRepository := user.NewRepository(dbConn.GetDB())
+	userService := user.NewService(userRepository)
+	userHandler := user.NewHandler(userService)
+
+	router.InitRouter(userHandler)
+	router.Start("0.0.0.0:8080")
 }
